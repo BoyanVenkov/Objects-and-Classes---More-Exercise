4ca4c334abd7c3b70a0aea063d0047972a644fb5
Boyan Venkov <boyanvenkov@yahoo.com>
2025-12-16 21:25:36 +0200

    Added a New Project

---
+++ 02. Oldest Family Member/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+}
+
+class Family
+{
+    private List<Person> people;
+
+    public Family()
+    {
+        people = new List<Person>();
+    }
+
+    public void AddMember(Person member)
+    {
+        people.Add(member);
+    }
+
+    public Person GetOldestMember()
+    {
+        return people.OrderByDescending(p => p.Age).First();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        Family family = new Family();
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] input = Console.ReadLine()
+                .Split();
+
+            string name = input[0];
+            int age = int.Parse(input[1]);
+
+            family.AddMember(new Person(name, age));
+        }
+
+        Person oldest = family.GetOldestMember();
+        Console.WriteLine($"{oldest.Name} {oldest.Age}");
+    }
+}
